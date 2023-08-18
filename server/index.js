605488b0dc080ab72619139be4e602af2e4784c4
Farkhod Kodirov <farxod94@gmail.com>
2023-08-19 02:21:21 +0500

    feat: add socket connection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,23 @@ const http = require("http");
 const socketIo = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+const activeSessions = {};
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
