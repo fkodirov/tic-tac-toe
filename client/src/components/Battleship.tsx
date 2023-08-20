5c45ab80f6e27c55d9614f97505ba3d99130310c
Farkhod Kodirov <farxod94@gmail.com>
2023-08-20 22:02:33 +0500

    feat: add boards and ability to place ship

--- client/src/components/Battleship.tsx
+++ client/src/components/Battleship.tsx
@@ -10,23 +10,37 @@ const Battleship: React.FC = () => {
   const [playerBoard, setPlayerBoard] = useState<string[][]>([]);
   const [enemyBoard, setEnemyBoard] = useState<string[][]>([]);
   const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [placingShip, setPlacingShip] = useState<boolean>(true);
 
   useEffect(() => {
-    const newBoard: string[][] = Array.from({ length: 10 }, () =>
+    const newBoardPlayer: string[][] = Array.from({ length: 10 }, () =>
       Array(10).fill("")
     );
-    setPlayerBoard(newBoard);
-    setEnemyBoard(newBoard);
+    const newBoardEnemy: string[][] = Array.from({ length: 10 }, () =>
+      Array(10).fill("")
+    );
+    setPlayerBoard(newBoardPlayer);
+    setEnemyBoard(newBoardEnemy);
 
     if (Store.socket) {
       Store.socket?.emit("join", Store.sessionId, Store.game, Store.playerName);
       Store.socket.on("connected", (playersNames: []) => {
-        console.log(playersNames);
         setPlayersNames(playersNames);
         setIsConnected(true);
       });
     }
   }, []);
+
+  const handleCellClick = (rowIndex: number, cellIndex: number) => {
+    if (placingShip) {
+      const newBoard = [...playerBoard];
+      if (playerBoard[rowIndex][cellIndex] != "S") {
+        console.log("Da");
+        newBoard[rowIndex][cellIndex] = "S";
+        setPlayerBoard(newBoard);
+      }
+    }
+  };
 
   return (
     <>
@@ -35,7 +49,7 @@ const Battleship: React.FC = () => {
         <div className="player-board">
           <h2>Your Board</h2>
           <h4>{Store.playerName}</h4>
-          <Board board={playerBoard} />
+          <Board board={playerBoard} onClick={handleCellClick} id={1} />
         </div>
         <div className="enemy-board">
           <h2>Enemy Board</h2>
@@ -46,12 +60,13 @@ const Battleship: React.FC = () => {
                   ? playersNames[1]
                   : playersNames[0]}
               </h4>
-              <Board board={enemyBoard} />
+              <Board board={enemyBoard} id={2} />
             </>
           ) : (
             <p>Waiting for opponent...</p>
           )}
         </div>
+        <button onClick={() => setPlacingShip(false)}>Start Game</button>
       </div>
     </>
   );
--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -2,15 +2,21 @@ import React from "react";
 
 interface BoardProps {
   board: string[][];
+  onClick?: (rowIndex: number, cellIndex: number) => void;
+  id: number;
 }
 
-const Board: React.FC<BoardProps> = ({ board }) => {
+const Board: React.FC<BoardProps> = ({ board, onClick, id }) => {
   return (
     <div className="board-battle">
       {board.map((row, rowIndex) => (
-        <div key={rowIndex} className="row-battle">
+        <div key={`${rowIndex}-${id}`} className="row-battle">
           {row.map((cell, cellIndex) => (
-            <div key={cellIndex} className="cell-battle">
+            <div
+              key={`${cellIndex}-${id}`}
+              className={`cell-battle ${cell === "S" ? "ship" : ""}`}
+              onClick={() => onClick && onClick(rowIndex, cellIndex)}
+            >
               {cell}
             </div>
           ))}
